cc4f8c3038537f65bcd4e33350be0fb74215c03f
Fabián Santander <hdmsantander@gmail.com>
2021-06-25 00:52:40 -0500

    Change location of config files

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,20 +14,20 @@ fi
 
 # First use SCC
 echo "Executing scc analysis"
-$SCC_EXECUTABLE --by-file --ci -i java -o $HOME/scc.csv -f csv
+$SCC_EXECUTABLE --by-file --ci -i java -o scc.csv -f csv
 
 # Check that the SCC output is usable
-if [[ ! -r $HOME/scc.csv ]]; then
+if [[ ! -r scc.csv ]]; then
     echo "Couldn't generate SCC output"
     exit 1
 fi
 
 # Then run depends
 echo "Executing depends analysis"
-java -jar $DEPENDS_JAR -s -p dot -d $HOME java ./src depends
+java -jar $DEPENDS_JAR -s -p dot -d ./ java ./src depends
 
 # Check that the depends output is usable
-if [[ ! -r $HOME/depends.json ]]; then
+if [[ ! -r depends.json ]]; then
     echo "Couldn't generate depends output"
     exit 1
 fi
@@ -39,25 +39,25 @@ git --version
 # Check if the input init date input is set, else use last month's commits
 if [ -n "$INPUT_INIT_DATE" ]; then
     echo "Using start date $INPUT_INIT_DATE"
-    git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat --after=$INPUT_INIT_DATE > $HOME/git.log
+    git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat --after=$INPUT_INIT_DATE > git.log
 else
     MIN_DATE=$(date +'%Y-%m-%d' -d 'last month')
     echo "Looking for commits since $MIN_DATE"
-    git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat --after=$MIN_DATE > $HOME/git.log
+    git log --pretty=format:'[%h] %an %ad %s' --date=short --numstat --after=$MIN_DATE > git.log
 fi
 
 # Check if the git log was generated
-if [[ ! -r  $HOME/git.log ]]; then
+if [[ ! -r  git.log ]]; then
     echo "Git log is empty, stopping..."
     exit 1
 else
     
     # Run the GitLog analyzer, min cochanges defaults to 0
-    java -jar $GITLOG_JAR -f $HOME/git.log > $HOME/frecuencies.csv
-    java -jar $GITLOG_JAR -f $HOME/git.log -coupling $INPUT_MIN_COCHANGES > $HOME/coupling.csv
+    java -jar $GITLOG_JAR -f git.log > frecuencies.csv
+    java -jar $GITLOG_JAR -f git.log -coupling $INPUT_MIN_COCHANGES > coupling.csv
     
     # Check if the git log analyzer output is usable and it was generated
-    if [[ ! -r $HOME/frecuencies.csv || ! -r $HOME/coupling.csv ]]; then
+    if [[ ! -r frecuencies.csv || ! -r coupling.csv ]]; then
         echo "Couldn't generate git log analyzer output"
         exit 1
     fi
